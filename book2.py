0a013208406cde4d4e5cb6cc006bca4c21d6d47a
Ulugbek <urakov344@gmail.com>
2025-02-13 16:36:16 +0500

    first commit

--- book2.py
+++ book2.py
@@ -20,6 +20,7 @@ books = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
     Book(2, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
     Book(3, 'Computer Science Pro', 'codingwithroby', 'A awesome book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book description', 3),
     Book(5, 'HP2', 'Author 2', 'Book description', 2),
     Book(6, 'HP3', 'Author 3', 'Book description', 1)
 ]
